69506f560eb942fd676be4c4e82d35d9e8a7ea7d
mathe-man <mathieumousset6@gmail.com>
2026-07-29 08:51:45 +0300

    Add solution and gitignore

---
+++ FlowResolver/FlowResolver.cs
@@ -0,0 +1,20 @@
+﻿namespace FlowResolver;
+
+using OpenCvSharp;
+
+public static class FlowResolver
+{
+    static VideoCapture OpenVideoSource(string filePath)
+    {
+        return new VideoCapture(filePath);
+    }
+
+    static VideoWriter OpenVideoOutput(string filePath, VideoCapture blueprint)
+    {
+        return new VideoWriter(
+            filePath, 
+            FourCC.FromFourChars('m', 'p', '4', 'v'), 
+            blueprint.Fps, 
+            new Size(blueprint.FrameWidth, blueprint.FrameHeight));
+    }
+}
